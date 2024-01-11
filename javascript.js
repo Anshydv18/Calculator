6805eda06c55c0a026b1abbf75836d198a8aec40
Anshydv18 <155738168+Anshydv18@users.noreply.github.com>
2024-01-11 10:45:05 +0000

    updated

--- javascript.js
+++ javascript.js
@@ -2,6 +2,7 @@ let res = document.getElementById('display');
 
 function append(value){
     res.value+=value;
+    playsound();
 }
 
 function backspace(){
@@ -9,13 +10,15 @@ function backspace(){
         res.value = '';
     }
     res.value = res.value.slice(0,-1);
-
+    playsound();
 }
 
 function saaf(){
     res.value='';
+    playsound();
 }
 function cal(){
+    playsound();
     try{
         if(res.value=='') {
             res.value='0';
@@ -26,4 +29,12 @@ function cal(){
     catch(e){
         res.value='ERROR';
     }
+}
+
+// function for playing sound
+
+function playsound(){
+    const music = document.getElementById('sound');
+    music.currentTime=0;
+    music.play();
 }
